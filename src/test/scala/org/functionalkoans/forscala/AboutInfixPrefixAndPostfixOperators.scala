eb0d22a2eb588ddaed918e1be9efafd05a48ff83
MatteoPierro <pierromatteo@gmail.com>
2016-07-12 13:56:21 +0200

    resolves AboutInfixPrefixAndPostfixOperators

--- src/test/scala/org/functionalkoans/forscala/AboutInfixPrefixAndPostfixOperators.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutInfixPrefixAndPostfixOperators.scala
@@ -10,22 +10,22 @@ class AboutInfixPrefixAndPostfixOperators extends KoanSuite with ShouldMatchers 
            |  has a method that takes one parameter.""") {
 
     val g: Int = 3
-    (g + 4) should be(__) // + is an infix operator
-     g.+(4) should be(__) // same result but not using the infix operator
+    (g + 4) should be(7) // + is an infix operator
+     g.+(4) should be(7) // same result but not using the infix operator
   }
 
   koan("""Infix Operators do NOT work if an object
            |  has a method that takes two parameters.""") {
     val g: String = "Check out the big brains on Brad!"
-    g indexOf 'o' should be(__) //indexOf(Char) can be used as an infix operator
+    g indexOf 'o' should be(6) //indexOf(Char) can be used as an infix operator
     //g indexOf 'o', 4 should be (6) //indexOf(Char, Int) cannot be used an infix operator
-    g.indexOf('o', 7) should be(__) //indexOf(Char, Int) must use standard java/scala calls
+    g.indexOf('o', 7) should be(25) //indexOf(Char, Int) must use standard java/scala calls
   }
 
   koan("""Postfix operators work if an object
            |  has a method that takes no parameters.""") {
     val g: Int = 31
-    (g toHexString) should be(__) //toHexString takes no params therefore can be called
+    (g toHexString) should be("1f") //toHexString takes no params therefore can be called
     //as a postfix operator. Hint: The answer is 1f
   }
 
@@ -33,7 +33,7 @@ class AboutInfixPrefixAndPostfixOperators extends KoanSuite with ShouldMatchers 
   koan("""Prefix operators work if an object
            |  has a method name that starts with unary_ .""") {
     val g: Int = 31
-    (-g) should be(__)
+    (-g) should be(-31)
   }
 
   koan("""Here we create our own prefix operator for our own class.
@@ -47,7 +47,7 @@ class AboutInfixPrefixAndPostfixOperators extends KoanSuite with ShouldMatchers 
     }
 
     val stereo = new Stereo
-    (+stereo) should be(__)
-    (-stereo) should be(__)
+    (+stereo) should be("on")
+    (-stereo) should be("off")
   }
 }
